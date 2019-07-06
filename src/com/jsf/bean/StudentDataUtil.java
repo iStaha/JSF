d87acf8e9ca29130b20195f6a9785999ab74790c
Sheikh Taha <tahalatief@gmail.com>
2019-07-06 10:00:17 +0530

    commit

---
+++ src/com/jsf/bean/StudentDataUtil.java
@@ -0,0 +1,32 @@
+package com.jsf.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+public class StudentDataUtil {
+
+	private List<Student> students;
+	
+	public StudentDataUtil() {
+		loadSampleData();
+	}
+	
+	public void loadSampleData() {
+		students = new ArrayList<>();
+		
+		/*
+		 * students.add(new Student("Mary", "Public", "mary@luv2code.com"));
+		 * students.add(new Student("John", "Doe", "john@luv2code.com"));
+		 * students.add(new Student("Ajay", "Rao", "ajay@luv2code.com"));
+		 */
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+}
